70a4e20e9f5f1c054b6842e0e219dbb974909f7a
SkibidiSigmasOfOhio <alberto_sanchez@student.davincischools.org>
2025-02-13 10:26:08 -0800

    Create ChallengeB.rb

---
+++ ChallengeB.rb
@@ -0,0 +1,71 @@
+use_synth :saw
+use_bpm 190
+#total sleep time = 9.5 Seconds
+live_loop :opening do
+  sample :drum_bass_hard
+  sleep 0.5
+  sample :drum_bass_hard
+  sleep 1
+  sample :drum_bass_hard
+  sleep 0.5
+  sample :drum_bass_hard
+  sleep 1
+  sample :drum_bass_hard
+  sleep 0.5
+  sample :drum_bass_hard
+  sleep 1
+  sample :drum_bass_hard
+  sleep 1
+  sample :drum_bass_hard
+  sleep 1
+  sample :drum_bass_hard
+  sleep 0.5
+  sample :drum_bass_hard
+  sleep 1
+  sample :drum_bass_hard
+  sleep 0.5
+end
+sleep 15
+#Total sleep time =
+use_bpm 165
+live_loop :stranger_things do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :c3
+  sleep 0.5
+  
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  
+  
+  play :c2
+  sleep 0.5
+  
+  play :e2
+  sleep 0.5
+  
+  play :g2
+  sleep 0.5
+  
+  play :b2
+  sleep 0.5
+  
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
